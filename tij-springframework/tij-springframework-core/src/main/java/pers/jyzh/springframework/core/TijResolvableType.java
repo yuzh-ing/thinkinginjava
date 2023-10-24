aa322353248cec3e2652e2421ae78f39f12315bb
yahya526 <1584558751@qq.com>
2023-10-24 13:56:31 +0800

    SpringFramework

--- tij-springframework/tij-springframework-core/src/main/java/pers/jyzh/springframework/core/TijResolvableType.java
+++ tij-springframework/tij-springframework-core/src/main/java/pers/jyzh/springframework/core/TijResolvableType.java
@@ -1,10 +1,16 @@
 package pers.jyzh.springframework.core;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.core.ResolvableType;
+import org.springframework.util.ReflectionUtils;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author ZhengYu
@@ -12,15 +18,35 @@ import org.springframework.core.ResolvableType;
  **/
 public class TijResolvableType {
 
-  private HashMap<Integer, List<String>> mp;
+  private List<Map<String, ClassA<Integer, ? extends String, ? super BigDecimal>>>[] F;
 
   public static void main(String[] args) throws Exception {
-    ResolvableType rt1 = ResolvableType.forClass(List.class);
-    ResolvableType rt2 = ResolvableType.forRawClass(ArrayList.class);
+    // 实例化：提供了工厂方法：ResolvableType.forXXX()
+    newInstance();
 
-    List<List<String>>[] arr = new List[0];
-    ResolvableType rt3 = ResolvableType.forInstance(arr);
-
+    Field field = ReflectionUtils.findField(TijResolvableType.class, "F");
     System.out.println();
+  }
+
+  private static void newInstance() {
+    // 取class的封装对象
+    ResolvableType rt1 = ResolvableType.forClass(ClassA.class);
+    ResolvableType rt2 = ResolvableType.forRawClass(ClassA.class);
+  }
+
+  @Getter
+  @Setter
+  public static class ClassA<T extends Number & Serializable, M extends String, N> {
+    private T f1;
+    private M f2;
+    private N f3;
+
+    public ClassA() {
+
+    }
+
+    public ClassA(T f1) {
+      this.f1 = f1;
+    }
   }
 }
