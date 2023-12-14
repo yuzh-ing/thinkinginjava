287870933a09e1e50acd459a89785d4e7583842c
zhengyu <zhengyu@hd123.com>
2023-12-14 17:35:14 +0800

    ByteCode

---
+++ tij-jvm/tij-jvm-bytecode/src/main/java/pers/jyzh/jvm/bytecode/asm/AsmMain.java
@@ -0,0 +1,11 @@
+package pers.jyzh.jvm.bytecode.asm;
+
+/**
+ * @author ZhengYu
+ * @version 1.0 2023/12/14 17:34
+ **/
+public class AsmMain {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ tij-jvm/tij-jvm-bytecode/src/main/java/pers/jyzh/jvm/bytecode/javassist/JavassistMain.java
@@ -0,0 +1,11 @@
+package pers.jyzh.jvm.bytecode.javassist;
+
+/**
+ * @author ZhengYu
+ * @version 1.0 2023/12/14 16:54
+ **/
+public class JavassistMain {
+  public static void main(String[] args) throws Exception {
+
+  }
+}
